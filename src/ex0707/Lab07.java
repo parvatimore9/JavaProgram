638c4d5efaa9e693718347471d0d0b472426f29a
Parvati <parvatimore9@gmail.com>
2024-08-27 17:29:25 +0530

    First commit

--- src/ex0707/Lab07.java
+++ src/ex0707/Lab07.java
@@ -1,4 +1,15 @@
 package ex0707;
 
 public class Lab07 {
+    //keywords - reserved words,all in lower case
+    //ex package,public.
+    // identifier- variable name
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+        System.out.print("Hello world");
+        System.out.print("Hello world");
+
+    }
 }
