4311b97a988bceee323d120142ffab73099bd75d
Parvati <parvatimore9@gmail.com>
2024-08-27 17:40:05 +0530

    First commit

---
+++ src/ex0707/Lab08.java
@@ -0,0 +1,8 @@
+package ex0707;
+
+public class Lab08 {
+    public static void main(String[] parvati)
+    {
+        System.out.println("args name can be anything");
+    }
+}
---
+++ src/ex0707/Lab09.java
@@ -0,0 +1,7 @@
+package ex0707;
+
+public class Lab09 {
+    public static void main(String args[]) {
+        System.out.println("bracket after args");
+    }
+}
